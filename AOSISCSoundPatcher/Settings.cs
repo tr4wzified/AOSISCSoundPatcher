ce89afce996519dd9515ae07c1659d97c25e6a03
trawzified <55751269+tr4wzified@users.noreply.github.com>
2023-03-02 18:19:10 +0100

    Fix some settings not coming through

--- AOSISCSoundPatcher/Settings.cs
+++ AOSISCSoundPatcher/Settings.cs
@@ -3,6 +3,8 @@ namespace AOSISCSoundPatcher
     public class Settings
     {
         public bool PatchArmors { get; set; }
+        public bool PatchWeapons { get; set; }
+        public bool PatchSoulGems { get; set; }
         public bool PatchMagicEffects { get; set; }
     }
 }
